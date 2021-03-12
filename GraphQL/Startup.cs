e1259d2215e90c1d093f7787a1a52d95131e3fbb
NiGHT <nightthexd@googlemail.com>
2021-03-12 23:40:36 +0100

    chore: add DbContext to Startup, Initial Migration / Create Database

--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -1,6 +1,8 @@
+using GraphQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +18,7 @@ namespace GraphQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
