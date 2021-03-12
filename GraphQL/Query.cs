09cb177961a32b175155dce83c2d21966d95fb05
NiGHT <nightthexd@googlemail.com>
2021-03-12 23:54:13 +0100

    chore: add query class, register services in Startup class feat: receive graphql speakers schema with banana cake pop

---
+++ GraphQL/Query.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Data;
+using HotChocolate;
+using System.Linq;
+
+namespace GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) =>
+             context.Speakers;
+    }
+}
--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -1,14 +1,9 @@
 using GraphQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraphQL
 {
@@ -18,7 +13,12 @@ namespace GraphQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=conferences.db"));
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlite("Data Source=conferences.db"));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,10 +33,7 @@ namespace GraphQL
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
         }
     }
