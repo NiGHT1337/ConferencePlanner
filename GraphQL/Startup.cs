b10009c2830565e86bf8cef4a1014710075eecd9
NiGHT <nightthexd@googlemail.com>
2021-03-13 00:05:23 +0100

    chore: add SpeakerInput, SpeakerPayload, Mutations, Services to Startup feat: add new Speakers to conferences.db

--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -18,7 +18,8 @@ namespace GraphQL
 
             services
                 .AddGraphQLServer()
-                .AddQueryType<Query>();
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
